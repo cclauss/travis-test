5e2ff6c21368d8992fd695089e858c5f8c6b5243
Denver Ogaro <ogaro@google.com>
2017-07-09 19:52:06 +0200

    Update bare_centos_preinstall.sh

--- bare_centos_preinstall.sh
+++ bare_centos_preinstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-yum install -y emacs epel-release python-pip python-devel wget which java-1.8.0-openjdk libffi-devel openssl-devel
+yum install -y emacs epel-release python-devel wget which java-1.8.0-openjdk libffi-devel openssl-devel
 yum groupinstall -y "Development Tools"
+yum install -y python-pip
 pip install virtualenv
 pip install --upgrade pip
