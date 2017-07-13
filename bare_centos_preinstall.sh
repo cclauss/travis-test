60281be90a0632b54511169d58bf5b0e2a78aaad
Denver Ogaro <ogaro@google.com>
2017-07-13 11:20:50 +0200

    First try. Will probably fail.

--- bare_centos_preinstall.sh
+++ bare_centos_preinstall.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-yum install -y emacs epel-release python-devel wget which java-1.8.0-openjdk libffi-devel openssl-devel
-yum groupinstall -y "Development Tools"
+yum install -y \
+  emacs \
+  epel-release \
+  python-devel \
+  wget \
+  which java-1.8.0-openjdk \
+  libffi-devel \
+  openssl-devel \
+  zip \
+  git \
+  gcc \
+  gcc-c++ \
+  redhat-rpm-config \
+  rpm-build \
+  rpm-sign \
 yum install -y python-pip
+# yum groupinstall -y "Development Tools"
 pip install virtualenv
-pip install --upgrade pip
