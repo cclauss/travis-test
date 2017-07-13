79f9d54e6f3e322c8b8bbccc1ddfdcb7d16760c3
Denver Ogaro <ogaro@google.com>
2017-07-13 10:17:49 +0200

    Stop building components

--- run_centos_docker_workflow.sh
+++ run_centos_docker_workflow.sh
@@ -20,6 +20,4 @@ source "${HOME}/INSTALL/bin/activate"
 
 grr_client_build build --output built_templates
 
-grr_client_build build_components --output built_templates
-
 grr_client_build --verbose --secondary_configs grr/config/grr-response-test/test_data/dummyconfig.yaml repack --template built_templates/*.zip --output_dir built_templates/
