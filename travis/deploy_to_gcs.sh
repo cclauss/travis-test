928822a72322a977d482cca95c0abd0d63aca257
Denver Ogaro <ogaro@google.com>
2017-07-16 18:12:34 +0200

    Deploy to test bucket.

--- travis/deploy_to_gcs.sh
+++ travis/deploy_to_gcs.sh
@@ -54,8 +54,8 @@ openssl aes-256-cbc -K "$encrypted_db009a5a71c6_key" \
   -out travis/travis_uploader_service_account.json -d
 
 gcloud auth activate-service-account --key-file travis/travis_uploader_service_account.json
-echo Uploading templates to "gs://autobuilds.grr-response.com/${TRAVIS_JOB_NUMBER}"
-gsutil -m cp built_templates/* "gs://autobuilds.grr-response.com/${TRAVIS_JOB_NUMBER}/"
+echo Uploading templates to "gs://ogaro-travis-test/${TRAVIS_JOB_NUMBER}"
+gsutil -m cp built_templates/* "gs://ogaro-travis-test/${TRAVIS_JOB_NUMBER}/"
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   shred -u travis/travis_uploader_service_account.json
