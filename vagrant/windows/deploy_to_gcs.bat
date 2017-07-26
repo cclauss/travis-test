afa8bbf413e58de8bb1b1cb484e05af93d86d511
Denver Ogaro <ogaro@google.com>
2017-07-26 17:45:52 +0200

    Remove quotes when defining vars.

--- vagrant/windows/deploy_to_gcs.bat
+++ vagrant/windows/deploy_to_gcs.bat
@@ -3,7 +3,7 @@ set PATH=%PATH%;C:\grr_deps\google-cloud-sdk\bin
 gcloud auth activate-service-account --key-file C:\grr_src\vagrant\windows\ogaro.appveyor-test.json
 
 :: TODO(ogaro): Sort.
-set GCE_DEST="gs://ogaro-travis-test/%APPVEYOR_REPO_COMMIT_TIMESTAMP%_%APPVEYOR_REPO_COMMIT%/appveyor_%APPVEYOR_BUILD_ID%_%APPVEYOR_JOB_ID%/"
+set GCE_DEST=gs://ogaro-travis-test/%APPVEYOR_REPO_COMMIT_TIMESTAMP%_%APPVEYOR_REPO_COMMIT%/appveyor_%APPVEYOR_BUILD_ID%_%APPVEYOR_JOB_ID%/
 echo Uploading templates to %GCE_DEST%
 
 ::gsutil -m cp C:\grr_src\output\* %GCE_DEST%
