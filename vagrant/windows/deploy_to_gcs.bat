d2cad573f8119553b07e2c1daf450f2e2ef2e2f6
Denver Ogaro <ogaro@google.com>
2017-07-26 18:49:31 +0200

    Use var again.

--- vagrant/windows/deploy_to_gcs.bat
+++ vagrant/windows/deploy_to_gcs.bat
@@ -2,10 +2,9 @@ set PATH=%PATH%;C:\grr_deps\google-cloud-sdk\bin
 
 powershell gcloud auth activate-service-account --key-file C:\grr_src\vagrant\windows\ogaro.appveyor-test.json
 
-:: TODO(ogaro): Sort.
-:: set GCE_DEST=gs://ogaro-travis-test/%APPVEYOR_REPO_COMMIT_TIMESTAMP%_%APPVEYOR_REPO_COMMIT%/appveyor_%APPVEYOR_BUILD_ID%_%APPVEYOR_JOB_ID%/
+set GCE_DEST=gs://ogaro-travis-test/%APPVEYOR_REPO_COMMIT_TIMESTAMP%_%APPVEYOR_REPO_COMMIT%/appveyor_build_%APPVEYOR_BUILD_NUMBER%_job_%APPVEYOR_JOB_NUMBER%/
 :: echo Uploading templates to gs://ogaro-travis-test/%APPVEYOR_REPO_COMMIT_TIMESTAMP%_%APPVEYOR_REPO_COMMIT%/appveyor_%APPVEYOR_BUILD_ID%_%APPVEYOR_JOB_ID%/
-echo Uploading templates to %APPVEYOR_REPO_COMMIT%
+echo Uploading templates to %GCE_DEST%
 
 ::gsutil -m cp C:\grr_src\output\* %GCE_DEST%
-gsutil -m cp C:\Python27-x64\python.exe "gs://ogaro-travis-test/%APPVEYOR_REPO_COMMIT_TIMESTAMP%_%APPVEYOR_REPO_COMMIT%/appveyor_%APPVEYOR_BUILD_ID%_%APPVEYOR_JOB_ID%/"
+powershell gsutil -m cp C:\Python27-x64\python.exe %GCE_DEST%
