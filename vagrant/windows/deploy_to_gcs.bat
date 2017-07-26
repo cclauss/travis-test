037aee4a77b200e73abd4f7235582d8ac83d49b5
Denver Ogaro <ogaro@google.com>
2017-07-26 17:15:12 +0200

    Actually upload templates.

--- vagrant/windows/deploy_to_gcs.bat
+++ vagrant/windows/deploy_to_gcs.bat
@@ -1,10 +1,8 @@
-rem TODO(ogaro): Move this to the installation stage.
-set PATH=%PATH%;C:\grr_deps\google-cloud-sdk\bin
-
-rem See https://www.appveyor.com/docs/how-to/secure-files/
+:: See https://www.appveyor.com/docs/how-to/secure-files/
 powershell gcloud auth activate-service-account --key-file C:\grr_src\vagrant\windows\ogaro.appveyor-test.json
 
-echo Uploading templates to "gs://ogaro-travis-test/appveyor"
+:: TODO(ogaro): Sort.
+set GCE_DEST "gs://ogaro-travis-test/%APPVEYOR_REPO_COMMIT%/appveyor_%APPVEYOR_BUILD_ID%_%APPVEYOR_JOB_ID%/"
+echo Uploading templates to %GCE_DEST%
 
-rem gsutil -m cp C:\grr_src\output\* "gs://ogaro-travis-test/appveyor"
-powershell gsutil -m cp C:\Python27-x64\python.exe "gs://ogaro-travis-test/appveyor"
+gsutil -m cp C:\grr_src\output\* %GCE_DEST%
