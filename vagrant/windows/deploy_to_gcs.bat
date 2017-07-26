cfc918ba8f06628b6efbc19cd4396b0ec3bd6af5
Denver Ogaro <ogaro@google.com>
2017-07-26 17:32:38 +0200

    Test creation of folders.

--- vagrant/windows/deploy_to_gcs.bat
+++ vagrant/windows/deploy_to_gcs.bat
@@ -1,9 +1,10 @@
 set PATH=%PATH%;C:\grr_deps\google-cloud-sdk\bin
 
-powershell gcloud auth activate-service-account --key-file C:\grr_src\vagrant\windows\ogaro.appveyor-test.json
+gcloud auth activate-service-account --key-file C:\grr_src\vagrant\windows\ogaro.appveyor-test.json
 
 :: TODO(ogaro): Sort.
-set GCE_DEST "gs://ogaro-travis-test/%APPVEYOR_REPO_COMMIT%/appveyor_%APPVEYOR_BUILD_ID%_%APPVEYOR_JOB_ID%/"
+set GCE_DEST="gs://ogaro-travis-test/%APPVEYOR_REPO_COMMIT_TIMESTAMP%_%APPVEYOR_REPO_COMMIT%/appveyor_%APPVEYOR_BUILD_ID%_%APPVEYOR_JOB_ID%/"
 echo Uploading templates to %GCE_DEST%
 
-gsutil -m cp C:\grr_src\output\* %GCE_DEST%
+::gsutil -m cp C:\grr_src\output\* %GCE_DEST%
+gsutil -m cp C:\Python27-x64\python.exe %GCE_DEST%
