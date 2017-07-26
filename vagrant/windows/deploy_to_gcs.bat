f3e1d293d619c84788d3cfb4798e30254c54b425
Denver Ogaro <ogaro@google.com>
2017-07-26 17:58:48 +0200

    Some commands are sdkipped?

--- vagrant/windows/deploy_to_gcs.bat
+++ vagrant/windows/deploy_to_gcs.bat
@@ -3,8 +3,9 @@ set PATH=%PATH%;C:\grr_deps\google-cloud-sdk\bin
 gcloud auth activate-service-account --key-file C:\grr_src\vagrant\windows\ogaro.appveyor-test.json
 
 :: TODO(ogaro): Sort.
-set GCE_DEST=gs://ogaro-travis-test/%APPVEYOR_REPO_COMMIT_TIMESTAMP%_%APPVEYOR_REPO_COMMIT%/appveyor_%APPVEYOR_BUILD_ID%_%APPVEYOR_JOB_ID%/
-echo Uploading templates to %GCE_DEST%
+:: set GCE_DEST=gs://ogaro-travis-test/%APPVEYOR_REPO_COMMIT_TIMESTAMP%_%APPVEYOR_REPO_COMMIT%/appveyor_%APPVEYOR_BUILD_ID%_%APPVEYOR_JOB_ID%/
+:: echo Uploading templates to gs://ogaro-travis-test/%APPVEYOR_REPO_COMMIT_TIMESTAMP%_%APPVEYOR_REPO_COMMIT%/appveyor_%APPVEYOR_BUILD_ID%_%APPVEYOR_JOB_ID%/
+echo Uploading templates to %APPVEYOR_REPO_COMMIT%
 
 ::gsutil -m cp C:\grr_src\output\* %GCE_DEST%
-gsutil -m cp C:\Python27-x64\python.exe %GCE_DEST%
+gsutil -m cp C:\Python27-x64\python.exe "gs://ogaro-travis-test/%APPVEYOR_REPO_COMMIT_TIMESTAMP%_%APPVEYOR_REPO_COMMIT%/appveyor_%APPVEYOR_BUILD_ID%_%APPVEYOR_JOB_ID%/"
