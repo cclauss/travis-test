6eee06412ef256eaeade5becb85fc595d20dba1b
Denver Ogaro <ogaro@google.com>
2017-07-26 20:15:13 +0200

    See if gsutil supports wildcards.

--- vagrant/windows/deploy_to_gcs.ps1
+++ vagrant/windows/deploy_to_gcs.ps1
@@ -9,8 +9,8 @@ $raw_commit_dt = [DateTime]$env:APPVEYOR_REPO_COMMIT_TIMESTAMP
 # Create a shorter, more readable time string.
 $short_commit_dt = $raw_commit_dt.ToString("yyyy-MM-ddTHH:mmUTC")
 
-$GCE_DEST = "gs://ogaro-travis-test/{0}_{1}/appveyor_build_{2}_job_{3}/" -f $short_commit_dt, $env:APPVEYOR_REPO_COMMIT, $env:APPVEYOR_BUILD_NUMBER, $env:APPVEYOR_JOB_NUMBER
+$gce_dest = "gs://ogaro-travis-test/{0}_{1}/appveyor_build_{2}_job_{3}/" -f $short_commit_dt, $env:APPVEYOR_REPO_COMMIT, $env:APPVEYOR_BUILD_NUMBER, $env:APPVEYOR_JOB_NUMBER
 
-echo "Uploading templates to $GCE_DEST"
+echo "Uploading templates to $gce_dest"
 
-gsutil -m cp "C:\Python27-x64\python.exe" $GCE_DEST
+gsutil -m cp "C:\grr_src\output\*" $gce_dest
