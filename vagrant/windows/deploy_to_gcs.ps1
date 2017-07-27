07c22282b6d8b7d30218ac991fc7431223a32d16
Denver Ogaro <ogaro@google.com>
2017-07-27 09:55:36 +0200

    Time upload.

--- vagrant/windows/deploy_to_gcs.ps1
+++ vagrant/windows/deploy_to_gcs.ps1
@@ -11,14 +11,22 @@ if (!$?) {
 $raw_commit_dt = [DateTime]$env:APPVEYOR_REPO_COMMIT_TIMESTAMP
 
 # Create a shorter, more readable time string.
-$short_commit_dt = $raw_commit_dt.ToString("yyyy-MM-ddTHH:mmUTC")
+$short_commit_timestamp = $raw_commit_dt.ToString("yyyy-MM-ddTHH:mmUTC")
 
-$gce_dest = "gs://ogaro-travis-test/{0}_{1}/appveyor_build_{2}_job_{3}/" -f $short_commit_dt, $env:APPVEYOR_REPO_COMMIT, $env:APPVEYOR_BUILD_NUMBER, $env:APPVEYOR_JOB_NUMBER
+$gce_dest = "gs://ogaro-travis-test/{0}_{1}/appveyor_build_{2}_job_{3}/" -f $short_commit_timestamp, $env:APPVEYOR_REPO_COMMIT, $env:APPVEYOR_BUILD_NUMBER, $env:APPVEYOR_JOB_NUMBER
 
 echo "Uploading templates to $gce_dest"
 
+$stop_watch = [Diagnostics.Stopwatch]::StartNew()
 gsutil cp "C:\grr_src\output\*" $gce_dest
-
 if (!$?) {
   exit 2
 }
+$stop_watch.Stop()
+$upload_duration = $stop_watch.Elapsed.TotalSeconds
+
+# gsutil will print an info message recommending using the -m option (parallel
+# object upload) when copying objects to GCP. For some reason however, that
+# doesn't seem to work properly on Appveyor. Some files fail to upload
+# with unhelpful error messages like 'Duplicate type [0:0:2]'
+echo "Sequential object upload took $copy_duration seconds"
