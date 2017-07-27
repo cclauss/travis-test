ed55584ed678ba955da5ae1b2295282615b87824
Denver Ogaro <ogaro@google.com>
2017-07-27 11:06:35 +0200

    Apply lint fixes.

--- vagrant/windows/deploy_to_gcs.ps1
+++ vagrant/windows/deploy_to_gcs.ps1
@@ -1,4 +1,4 @@
-$env:PATH += ";C:\grr_deps\google-cloud-sdk\bin"
+$env:PATH += ';C:\grr_deps\google-cloud-sdk\bin'
 
 gcloud auth activate-service-account --key-file C:\grr_src\vagrant\windows\ogaro.appveyor-test.json
 
@@ -11,14 +11,14 @@ if (!$?) {
 $raw_commit_dt = [DateTime]$env:APPVEYOR_REPO_COMMIT_TIMESTAMP
 
 # Create a shorter, more readable time string.
-$short_commit_timestamp = $raw_commit_dt.ToString("yyyy-MM-ddTHH:mmUTC")
+$short_commit_timestamp = $raw_commit_dt.ToString('yyyy-MM-ddTHH:mmUTC')
 
-$gce_dest = "gs://ogaro-travis-test/{0}_{1}/appveyor_build_{2}_job_{3}/" -f $short_commit_timestamp, $env:APPVEYOR_REPO_COMMIT, $env:APPVEYOR_BUILD_NUMBER, $env:APPVEYOR_JOB_NUMBER
+$gce_dest = 'gs://ogaro-travis-test/{0}_{1}/appveyor_build_{2}_job_{3}/' -f $short_commit_timestamp, $env:APPVEYOR_REPO_COMMIT, $env:APPVEYOR_BUILD_NUMBER, $env:APPVEYOR_JOB_NUMBER
 
-echo "Uploading templates to $gce_dest"
+Write-Output "Uploading templates to $gce_dest"
 
 $stop_watch = [Diagnostics.Stopwatch]::StartNew()
-gsutil cp "C:\grr_src\output\*" $gce_dest
+gsutil cp 'C:\grr_src\output\*' $gce_dest
 if (!$?) {
   exit 2
 }
@@ -29,4 +29,4 @@ $upload_duration = $stop_watch.Elapsed.TotalSeconds
 # object upload) when copying objects to GCP. For some reason however, that
 # doesn't seem to work properly on Appveyor. Some files arbitrarily fail to
 # upload with unhelpful error messages like 'Duplicate type [0:0:2]'
-echo "Sequential object upload took $upload_duration seconds"
+Write-Output "Sequential object upload took $upload_duration seconds"
