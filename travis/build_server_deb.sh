0d35d69c51e25ca6249d4bccca01d67513495b33
Denver Ogaro <ogaro@google.com>
2017-08-02 13:13:53 +0200

    Split build script into two.

--- travis/build_server_deb.sh
+++ travis/build_server_deb.sh
@@ -4,35 +4,6 @@ set -e
 
 TRAVIS_COMMIT=${TRAVIS_COMMIT:-$(git show -s --format='%H' HEAD)}
 
-function build_sdists() {
-  if [[ -d sdists ]]; then
-    echo "Removing existing sdists directory."
-    rm -rf sdists
-  fi
-
-  # TODO(ogaro): Make docs?
-  python setup.py sdist --formats=zip --dist-dir=$PWD/sdists --no-make-docs --no-sync-artifacts
-  python api_client/python/setup.py sdist --formats=zip --dist-dir=$PWD/sdists
-  python grr/config/grr-response-test/setup.py sdist --formats=zip --dist-dir=$PWD/sdists
-  python grr/config/grr-response-client/setup.py sdist --formats=zip --dist-dir=$PWD/sdists
-  #python grr/config/grr-response-templates/setup.py sdist --formats=zip --dist-dir=$PWD/sdists
-  python grr/config/grr-response-server/setup.py sdist --formats=zip --dist-dir=$PWD/sdists
-}
-
-function download_packages() {
-  if [[ -d local_pypi ]]; then
-    echo "Removing existing local_pypi directory."
-    rm -rf local_pypi
-  fi
-
-  pip download --dest=local_pypi sdists/grr-response-core-*.zip
-  pip download --dest=local_pypi sdists/grr-api-client-*.zip
-  pip download --dest=local_pypi sdists/grr-response-test-*.zip
-  pip download --dest=local_pypi sdists/grr-response-client-*.zip
-  #pip download --dest=local_pypi sdists/grr-response-templates-*.zip
-  pip download --dest=local_pypi sdists/grr-response-server-*.zip
-}
-
 function create_changelog() {
   if [[ -f debian/changelog ]]; then
     echo "Replacing debian/changelog with new changelog."
@@ -70,9 +41,6 @@ function export_build_vars() {
   export SERVER_SDIST="$(ls sdists | grep -e 'grr-response-server-.*\.zip')"
 }
 
-source "/home/${DOCKER_USER}/INSTALL/bin/activate"
-build_sdists
-download_packages
 create_changelog
 export_build_vars
 dpkg-buildpackage -us -uc
