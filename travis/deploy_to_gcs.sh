110a07aa615105ec6748f6f3852bfe883b98e2fc
Denver Ogaro <ogaro@google.com>
2017-11-29 18:43:21 +0100

    Move server deb upload logic to a script.

--- travis/deploy_to_gcs.sh
+++ travis/deploy_to_gcs.sh
@@ -23,3 +23,25 @@ fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   srm -sz travis/travis_uploader_service_account.json
 fi
+
+if [[ "${GCS_TAG}" != 'server_deb' ]]; then
+  exit 0
+fi
+
+# Bail if the server deb wasn't built.
+if [[ -z "$(ls gcs_upload_dir/grr-server_*_amd64.deb 2>/dev/null)" ]]; then
+  echo 'Server deb not found in gcs_upload_dir'
+  exit 1
+fi
+
+gsutil rm -r gs://${GCS_BUCKET}/_server_deb_temp/ || true
+
+gsutil -m cp gcs_upload_dir/* gs://${GCS_BUCKET}/_server_deb_temp
+
+gsutil rm -r gs://${GCS_BUCKET}/_latest_server_deb/ || true
+
+gsutil -m cp gs://${GCS_BUCKET}/_server_deb_temp/* gs://${GCS_BUCKET}/_latest_server_deb
+
+# Trigger build of a new GRR Docker image (grrdocker/grr)
+# See https://hub.docker.com/r/grrdocker/grr/~/settings/automated-builds/
+curl -H "Content-Type: application/json" --data '{"docker_tag": "latest"}' -X POST https://registry.hub.docker.com/u/grrdocker/grr/trigger/4499c4d4-4a8b-48da-bc95-5dbab39be545/
