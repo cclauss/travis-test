079044d03f2253f2ff3a7b1d4ca60324db30de1d
Denver Ogaro <ogaro@google.com>
2018-06-12 11:45:02 +0200

    Try posting to appveyor api.

---
+++ appveyor/linux/test_appveyor_api.py
@@ -0,0 +1,20 @@
+import os
+import requests
+import urlparse
+
+api_url = os.environ.get("APPVEYOR_API_URL")
+print("Using Appveyor API url %s" % api_url)
+
+if api_url:
+  tests_endpoint = urlparse.urljoin(api_url, "api/tests")
+  resp = requests.post(tests_endpoint, data={
+    "testName": "Dummy Test",
+    "testFramework": "JUnit",
+    "fileName": os.path.relpath(__file__),
+    "outcome": "Passed",
+    "durationMilliseconds": "1200",
+    "ErrorMessage": "",
+    "ErrorStackTrace": "",
+    "StdOut": "",
+    "StdErr": ""
+  })
