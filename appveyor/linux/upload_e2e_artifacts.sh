195793a42eccee615aa03801b4ae76af1c3ff067
Denver Ogaro <ogaro@google.com>
2018-06-20 13:14:02 +0200

    Allow ssh.

--- appveyor/linux/upload_e2e_artifacts.sh
+++ appveyor/linux/upload_e2e_artifacts.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -ex
+
 mkdir server-configs server-logs client-configs client-logs
-sudo cp /usr/share/grr-server/install_data/etc/grr-server.yaml server-configs || true # Primary server config.
-sudo cp /etc/grr/server.local.yaml server-configs || true # Server writeback.
-sudo cp /usr/share/grr-server/lib/python2.7/site-packages/grr/var/log/* server-logs || true
-sudo cp /usr/lib/grr/grr_*_amd64/grrd.yaml client-configs || true # Primary client config.
-sudo cp /etc/grr.local.yaml client-configs || true # Secondary client config.
-sudo cp /var/log/GRRlog.txt client-logs || true
+sudo cp /usr/share/grr-server/install_data/etc/grr-server.yaml server-configs/ || true # Primary server config.
+sudo cp /etc/grr/server.local.yaml server-configs/ || true # Server writeback.
+sudo cp /usr/share/grr-server/lib/python2.7/site-packages/grr/var/log/* server-logs/ || true
+sudo cp /usr/lib/grr/grr_*_amd64/grrd.yaml client-configs/ || true # Primary client config.
+sudo cp /etc/grr.local.yaml client-configs/ || true # Secondary client config.
+sudo cp /var/log/GRRlog.txt client-logs/ || true
 
 # Give read permissions to the non-root user.
 sudo chown -R "$(whoami)" server-configs server-logs client-configs client-logs
@@ -14,5 +16,6 @@ sudo chown -R "$(whoami)" server-configs server-logs client-configs client-logs
 appveyor PushArtifact e2e.log -DeploymentName 'Test Output'
 
 for cfg in "$(ls server-configs)"; do
-  appveyor PushArtifact "server-configs/${cfg}" -DeploymentName 'Server Configs'
+  echo "server-configs/${cfg}"
+  #appveyor PushArtifact "server-configs/${cfg}" -DeploymentName 'Server Configs'
 done
