5450edbf0192f62583e853f8cda4d20e65920ffd
Denver Ogaro <ogaro@google.com>
2018-06-20 13:43:47 +0200

    Group artifact dirs.

--- appveyor/linux/upload_e2e_artifacts.sh
+++ appveyor/linux/upload_e2e_artifacts.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
+readonly INITIAL_DIR="${PWD}"
+
+mkdir appveyor_e2e_artifacts
+cd appveyor_e2e_artifacts
+
 mkdir server-configs server-logs client-configs client-logs
 sudo cp /usr/share/grr-server/install_data/etc/grr-server.yaml server-configs/ || true # Primary server config.
 sudo cp /etc/grr/server.local.yaml server-configs/ || true # Server writeback.
@@ -11,11 +16,15 @@ sudo cp /etc/grr.local.yaml client-configs/ || true # Secondary client config.
 sudo cp /var/log/GRRlog.txt client-logs/ || true
 
 # Give read permissions to the non-root user.
-sudo chown -R "$(whoami)" server-configs server-logs client-configs client-logs
+sudo chown -R "$(whoami):$(whoami)" server-configs server-logs client-configs client-logs
+
+cd "${INITIAL_DIR}"
 
 appveyor PushArtifact e2e.log -DeploymentName 'Test Output'
 
-for cfg in "$(ls server-configs)"; do
-  echo "server-configs/${cfg}"
-  #appveyor PushArtifact "server-configs/${cfg}" -DeploymentName 'Server Configs'
-done
+appveyor PushArtifact appveyor_e2e_artifacts/server-configs/grr-server.yaml -DeploymentName 'Server Configs'
+
+#for cfg in "$(ls server-configs)"; do
+#  echo "server-configs/${cfg}"
+#  #appveyor PushArtifact "server-configs/${cfg}" -DeploymentName 'Server Configs'
+#done
