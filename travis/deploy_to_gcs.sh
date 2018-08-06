5346dc4d1c1b61a0a47b09170b0845fb5fe96009
Denver Ogaro <ogaro@google.com>
2018-08-06 17:22:50 +0200

    Fix sync_grr.sh.

--- travis/deploy_to_gcs.sh
+++ travis/deploy_to_gcs.sh
@@ -42,7 +42,7 @@ if [[ "${GCS_TAG}" == 'ubuntu_64bit' ]]; then
     --output win_template_response.txt \
     --header "Authorization: Bearer ${APPVEYOR_TOKEN}" \
     --header 'Content-Type: application/json' \
-    --data '{"accountName":"grr", "projectSlug":"grr-5wmt5", "branch":"master", "commitId":"'"${TRAVIS_COMMIT}"'"}' \
+    --data '{"accountName":"demonchild2112", "projectSlug":"travis-test-cij3q", "branch":"master", "commitId":"'"${TRAVIS_COMMIT}"'"}' \
     --request POST \
     https://ci.appveyor.com/api/builds 1>win_template_response_code.txt
 
@@ -96,7 +96,7 @@ curl --write-out %{http_code} \
   --output e2e_response.txt \
   --header "Authorization: Bearer ${APPVEYOR_TOKEN}" \
   --header 'Content-Type: application/json' \
-  --data '{"accountName":"grr", "projectSlug":"grr", "branch":"master", "commitId":"'"${TRAVIS_COMMIT}"'"}' \
+  --data '{"accountName":"demonchild2112", "projectSlug":"grr-1r2v0", "branch":"master", "commitId":"'"${TRAVIS_COMMIT}"'"}' \
   --request POST \
   https://ci.appveyor.com/api/builds 1>e2e_response_code.txt
 response_code="$(cat e2e_response_code.txt)"
